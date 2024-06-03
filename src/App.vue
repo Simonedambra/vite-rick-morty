4c2a703196a085af182383a9674a0743d6f80bec
Simonedambra <simonedambra130102@gmail.com>
2024-06-03 16:07:57 +0200

    upd: added img

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <!-- <script></script> -->
+<script>
+import appheader from './components/AppHeader.vue';
+
+export default{
+  data(){
+
+  },
+
+  components:{
+appheader,
+
+  }
+}
+</script>
 <template>
+  <appheader/>
   <h1>Vue Vite Template</h1>
   <p>Template di partenza per progetti Vite Vue</p>
 </template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name:'appheader',
+}
+
+</script>
+<template>
+<div class="container">
+<h1>logo</h1>
+
+<input type="text">
+<select name="cars" id="cars">
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+</select>
+<button>s</button>
+<button>s</button>
+</div>
+
+</template>
